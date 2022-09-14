78ceee3d621652ae06074cf853f1cb580ddd8bc9
Rajjmayank <111038504+Rajjmayank@users.noreply.github.com>
2022-09-14 21:08:17 +0530

    Create project.js

---
+++ project.js
@@ -0,0 +1,43 @@
+let screen = document.getElementById('screen');
+let result = document.getElementById('result');
+let resultValue = '';
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+equalPresed = false;
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        if (e.target.value == "C") {
+            resultValue = '';
+            result.value = resultValue;
+            screenValue = '';
+            screen.value = screenValue;
+
+        } else if (e.target.value == "dlt") {
+            screenValue = screenValue.substring(0, screenValue.length - 1);
+            screen.value = screenValue;
+        } else if (e.target.value == "=") {
+            result.value = eval(screenValue);
+            equalPresed = true;
+        } else if (equalPresed) {
+            if (e.target.value == "+" || e.target.value == "-" || e.target.value == '*' || e.target.value == "/") {
+                screenValue += e.target.value;
+                screen.value = screenValue;
+                equalPresed = false;
+            } else {
+                resultValue = '';
+                result.value = resultValue;
+                screenValue = '';
+                screen.value = screenValue;
+                screenValue += e.target.value;
+                screen.value = screenValue;
+                equalPresed = false;
+            }
+        } else {
+            screenValue += e.target.value;
+            screen.value = screenValue;
+            equalPresed = false;
+        }
+
+
+    });
+}
